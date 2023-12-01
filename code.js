c3cc937b9313ab2927ed007e521c0d81eeb8f73c
Adam McCondochie <47722627+AdamMcCondochie@users.noreply.github.com>
2023-12-01 00:03:43 +0000

    Update Index.html with animations, fix iframe scaling

---
+++ code.js
@@ -0,0 +1,46 @@
+document.addEventListener('DOMContentLoaded', function() {
+    circle = document.getElementById("mouse-circle");
+    circleInner = document.getElementById("mouse-circle-content");
+
+    lastMouseX = 0;
+    lastMouseY = 0;
+
+    posx = 0;
+    posy = 0;
+
+    document.onmousemove = handleMouseMove;
+    document.onmousedown = handleMouseDown;
+    document.onmouseup = handleMouseUp;
+
+    setInterval(updateMouseFollow, 25)
+ }, false);
+
+
+function handleMouseMove(event){
+    var parent = circle.parentNode;
+    var rect = parent.getBoundingClientRect();
+    var cRect = circleInner.getBoundingClientRect();
+
+    lastMouseX = event.clientX - rect.left - (cRect.width/2);
+    lastMouseY = event.clientY - rect.top - (cRect.height/2);
+}
+
+function updateMouseFollow(){
+    posx += (lastMouseX - posx) * .25 ;    
+    posy += (lastMouseY - posy) * .25 ;    
+
+    circle.style.left = posx;
+    circle.style.top = posy;
+}
+
+function handleMouseDown(event) {
+    //var circle = document.getElementById("mouse-circle");
+
+    circleInner.classList.add("mouse-circle-grow");
+}
+
+function handleMouseUp(event) {
+    //var circle = document.getElementById("mouse-circle");
+
+    circleInner.classList.remove("mouse-circle-grow");
+}
